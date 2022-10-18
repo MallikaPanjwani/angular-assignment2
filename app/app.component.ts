090f8f32db20c15dc08077f27328371e9159ea27
MallikaPanjwani <75017323+MallikaPanjwani@users.noreply.github.com>
2022-10-18 17:18:56 +0530

    3 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -16,30 +16,55 @@ export class AppComponent {
   title = 'Welcome to the world of books';
   searchText;
   books = [
-    { id: 11, name: 'The India Story', author: 'Bimal Jalal' },
+    {
+      id: 11,
+      name: 'The India Story',
+      author: 'Bimal Jalal',
+      date: '20 Jun 2022',
+    },
     {
       id: 12,
       name: 'Listen to Your Heart: The London Adventure',
       author: 'Ruskin Bond',
+      date: '20 Jun 2022',
     },
-    { id: 13, name: 'Business of Sports', author: 'Vinit Karnik' },
+
+    {
+      id: 13,
+      name: 'Business of Sports',
+      author: 'Vinit Karnik',
+      date: '20 Jun 2022',
+    },
     {
       id: 14,
       name: 'The Winning Formula for Success',
       author: 'Preeti Shenoy',
+      date: '20 Jun 2022',
     },
-    { id: 15, name: 'A Place Called Home', author: 'VP Venkaiah Naidu' },
-    { id: 16, name: 'Dreams Meeting Delivery', author: 'Rajnath Singh' },
-    { id: 17, name: 'Modi @20', author: 'Venkaiah Naidu' },
+    {
+      id: 15,
+      name: 'A Place Called Home',
+      author: 'VP Venkaiah Naidu',
+      date: '20 Jun 2022',
+    },
+    {
+      id: 16,
+      name: 'Dreams Meeting Delivery',
+      author: 'Rajnath Singh',
+      date: '20 Jun 2022',
+    },
+    { id: 17, name: 'Modi @20', author: 'Venkaiah Naidu', date: '20 Jun 2022' },
     {
       id: 18,
       name: 'The Struggle for Police Reforms in India',
       author: 'Ex-IPS Prakash Singh',
+      date: '20 Jun 2022',
     },
     {
       id: 19,
       name: 'INDO-PAK WAR 1971- Reminiscences of Air Warriors',
       author: 'Rajnath Singh',
+      date: '20 Jun 2022',
     },
   ];
 }
